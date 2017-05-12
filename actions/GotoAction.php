20576cee99c3874255166a6186819b18c5c7354e
Daniel Gerards <daniel.gerards@innogames.com>
2017-05-12 20:49:57 +0200

    updated code to be able to handle wood 1 rule changes

--- actions/GotoAction.php
+++ actions/GotoAction.php
@@ -2,9 +2,11 @@
 
 class GotoAction implements ActionInterface
 {
-    // Collect sample data files from the cloud at the DIAGNOSIS module.
+    // Collect sample data files from the cloud at the SAMPLES module.
+    // Diagnose sample data files at the DIAGNOSIS module to get their cost.
     // Gather required molecules for the medicines at the MOLECULES module.
     // Produce the medicines at the LABORATORY module and collect your health points.
+    const MODULE_SAMPLES    = 'SAMPLES';
     const MODULE_DIAGNOSIS  = 'DIAGNOSIS';
     const MODULE_MOLECULES  = 'MOLECULES';
     const MODULE_LABORATORY = 'LABORATORY';
--- entities/Sample.php
+++ entities/Sample.php
@@ -2,10 +2,15 @@
 
 class Sample
 {
-
     const CARRY_FLAG_OWN_ROBOT   = 0;
     const CARRY_FLAG_OTHER_ROBOT = 1;
     const CARRY_FLAG_CLOUD       = -1;
+
+    const RANK_1             = 1;
+    const RANK_2             = 2;
+    const RANK_3             = 3;
+
+    const COST_NOT_DIAGNOSED = -1;
 
     /**
      * unique id of the sample
@@ -48,5 +53,16 @@ class Sample
      * @var int[]
      */
     public $cost = [];
+
+    /**
+     * check if the sample was already diagnosed
+     * if not yet diagnosed, cost will be set to -1 for all molecules
+     *
+     * @return bool
+     */
+    public function isDiagnosed()
+    {
+        return reset($this->cost) > self::COST_NOT_DIAGNOSED;
+    }
 }
 
